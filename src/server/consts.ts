c4cba0fedb1e10b1468093ef4d72ed6ac19d1cbe
Mihir Patel <mihirkpatel9702@gmail.com>
2022-03-08 18:22:26 -0500

    heroku

--- src/server/consts.ts
+++ src/server/consts.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
 
-export const MONGO_URI = process.env.MONGO_URI || process.env.MONGO_LOCAL
+export const MONGO_URI =
+  'mongodb+srv://Mihir:Mihir@cluster0.6y5oo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 export const PORT = process.env.PORT || 5002
